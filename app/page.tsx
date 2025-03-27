9311d9eea23238f6aab358239d5198ffb42d7497
Eli Filiba <efiliba@hotmail.com>
2025-03-27 22:30:25 +1100

    Show differences between Three.js and React 3 Fiber

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,148 @@
-import Image from "next/image";
+"use client";
+
+// https://www.youtube.com/watch?v=DPl34H2ISsk&t=329s
+
+import { useRef, useEffect } from "react";
+import {
+  WebGLRenderer,
+  Scene,
+  PerspectiveCamera as ThreePerspectiveCamera,
+  MeshStandardMaterial,
+  Mesh,
+  BoxGeometry,
+  AmbientLight,
+  PointLight,
+  ACESFilmicToneMapping,
+  SRGBColorSpace,
+} from "three";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { PerspectiveCamera } from "@react-three/drei";
+
+const createMesh = () => {
+  const mesh = new Mesh();
+
+  const geometry = new BoxGeometry();
+
+  const material = new MeshStandardMaterial();
+
+  mesh.geometry = geometry;
+  mesh.material = material;
+
+  return mesh;
+};
+
+class Cube extends Mesh {
+  constructor() {
+    super();
+
+    const geometry = new BoxGeometry();
+    const material = new MeshStandardMaterial({ color: "blue" });
+
+    this.geometry = geometry;
+    this.material = material;
+  }
+
+  update() {
+    this.rotation.x += 0.01;
+    this.rotation.y += 0.01;
+  }
+
+  dispose() {
+    this.geometry.dispose(); // Done automatically in React version
+  }
+}
+
+const CubeComponent = () => {
+  const meshRef = useRef<Mesh>(null);
+
+  // Equivalent to update - called every frame (requestAnimationFrame)
+  useFrame(() => {
+    meshRef.current!.rotation.x += 0.01;
+    meshRef.current!.rotation.y += 0.01;
+  });
+
+  return (
+    <mesh ref={meshRef}>
+      <boxGeometry />
+      {/* <meshStandardMaterial color="blue" /> */}
+      <meshStandardMaterial>
+        <color args={["blue"]} attach="color" />
+      </meshStandardMaterial>
+    </mesh>
+  );
+};
+
+const vanillaScene = (width: number, height: number) => {
+  const animate = () => {
+    requestAnimationFrame(animate); // recursive
+    renderer.render(scene, camera);
+
+    // mesh.rotation.x += 0.01;
+    // mesh.rotation.y += 0.01;
+    mesh.update();
+  };
+
+  const scene = new Scene();
+
+  // Set to R3F defaults
+  const camera = new ThreePerspectiveCamera(75, width / height, 0.1, 1000);
+  camera.position.z = 5;
+
+  const ambientLight = new AmbientLight();
+  scene.add(ambientLight);
+
+  const pointLight = new PointLight();
+  pointLight.position.set(10, 10, 10);
+  scene.add(pointLight);
+
+  // const mesh = createMesh();
+  const mesh = new Cube();
+  scene.add(mesh);
+
+  const renderer = new WebGLRenderer({ alpha: true, antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.toneMapping = ACESFilmicToneMapping;
+  renderer.outputColorSpace = SRGBColorSpace;
+  renderer.setSize(width, height);
+
+  return { rendererDomElement: renderer.domElement, animate };
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const rootRef = useRef<HTMLDivElement>(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  // const container = document.getElementById("__next");
+  useEffect(() => {
+    console.log("container", rootRef.current?.clientWidth);
+
+    const { clientWidth, clientHeight } = rootRef.current!;
+    const { rendererDomElement, animate } = vanillaScene(
+      clientWidth,
+      clientHeight
+    );
+
+    rootRef.current?.appendChild(rendererDomElement);
+    animate();
+  }, []);
+
+  return (
+    <div className="m-4 grid grid-cols-2 justify-items-center gap-2">
+      <h1>Three.js</h1>
+      <h1>React Three Fiber</h1>
+      <div ref={rootRef} className="aspect-square border-1" />
+      {/* <Canvas camera={{ position: [0, 0, 5] }} gl={{ toneMapping: ACESFilmicToneMapping}}> */}
+      <Canvas className="aspect-square border-1">
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        {/* <PerspectiveCamera makeDefault fov={75} position={[0, 0, 5]} /> */}
+        {/* <mesh>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh> */}
+        {/* <boxGeometry attach="geometry" /> */}
+        {/* <boxGeometry args={[2, 2, 2]} /> // args added to constructor: i.e. new BoxGeometry(2, 2, 2) */}
+        <CubeComponent />
+      </Canvas>
     </div>
   );
 }
